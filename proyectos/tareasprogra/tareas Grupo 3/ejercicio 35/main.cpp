dbba2ce0bf7aae586ae8a2735cf4f709e950f09e
ArletheMendoza <arlethemendoza2010@gmail.com>
2022-02-22 00:11:39 -0600

    Tarea de robot con archivos

---
+++ proyectos/tareasprogra/tareas Grupo 3/ejercicio 35/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+{
+    nt numeroCuenta;
+    string nombre;
+    string apellido;
+
+    //Esto es un constructor para Vehiculo
+        saldo  (int c, string n, string a) {
+        numeroCuenta=c;
+        nombre=n;
+        apellido=a;
+       }
+
+     //Retornando el rango
+    int rango(){
+        return saldo*c;
+}
